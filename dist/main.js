c94c39ea21973342ef5a264e88128e3c2a4c1611
Chen Ozer <chen572@gmail.com>
2020-07-21 12:45:44 +0300

    intergrated mtrlCSS

--- dist/main.js
+++ dist/main.js
@@ -19,13 +19,13 @@ const handleSearch = async (cityName) => {
 }
 
 $('.search-btn').click(() => {
-    const cityName = $('.city-search').val()
-    $('.city-search').val('')
+    const cityName = $('#city-search').val()
+    $('#city-search').val('')
     handleSearch(cityName)
 })
 
 $('.bottom-container').on('click', '.remove, .add', async event => {
-    const city = model.findInCityDataArr($(event.currentTarget).siblings('.city-name').find('h2').text())
+    const city = model.findInCityDataArr($(event.currentTarget).siblings('#city-name').find('h2').text())
     if ($(event.currentTarget).hasClass('add')) {
         await model.saveCity(city)
         renderer.render({ city: model.cityData }, 'bottom')
